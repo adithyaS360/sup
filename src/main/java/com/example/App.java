f1b01d47f60d734758b5ca2207ad120a88ac5482
adithyaS360 <adithya12.s2001@gmail.com>
2026-06-12 19:59:38 +0530

    Refactor package declaration in App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,4 +1,4 @@
-package com.example.App;
+package com.example;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
